c497317bfe4ead36f7e1b4f9964d95fa04520391
Łukasz Czarnota <czarnymwkp@gmail.com>
2025-09-16 14:32:04 +0200

    adedd test <p>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,6 +9,12 @@ export const HomePage = () => {
         libero placeat, earum ea nostrum adipisci sunt consectetur facere,
         veritatis eos quia veniam ex dolores quaerat sint nobis nisi! Quas, est!
       </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        iusto neque possimus fugit, consectetur sequi similique sint
+        consequuntur esse inventore, repellendus nesciunt reiciendis, quasi
+        aperiam dolores corrupti ratione molestias sapiente!
+      </p>
       <UserDemo />
     </div>
   );
